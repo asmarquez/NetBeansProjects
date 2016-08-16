00ca875abf89d49e3ddf904179dcfe641971785a
Juan Francisco Chango <jchango@udlanet.ec>
2016-08-16 09:11:12 -0500

    agregar otra vez

---
+++ cheer_prueba/src/ch/makery/address/MainApp.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.makery.address;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author mami
+ */
+public class MainApp extends Application{
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    
+    public void start(Stage primaryStage){
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Cheervidetienda");
+        
+        iniciar_RootLayout();
+        mostrar_ventas();
+        
+    }
+    
+    
+    public void iniciar_RootLayout(){
+        
+        try {
+        //Cargar FMXL
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+        rootLayout = (BorderPane) loader.load();
+        
+        //Mostrar la escena denstro del root Layout
+        Scene scene = new Scene(rootLayout);
+        primaryStage.setScene(scene);
+        //primaryStage.setResizable(false); //No se modifica el tamaño
+        primaryStage.show();
+        
+        
+        
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    public void mostrar_ventas(){
+        try{
+            FXMLLoader loader = new FXMLLoader ();
+            loader.setLocation(MainApp.class.getResource("view/ventas.fxml"));
+            AnchorPane ventas = (AnchorPane) loader.load();
+            
+            rootLayout.setCenter(ventas);
+          
+            
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+        public void mostrar_compras(){
+        try{
+            FXMLLoader loader = new FXMLLoader ();
+            loader.setLocation(MainApp.class.getResource("view/compras.fxml"));
+            AnchorPane ventas = (AnchorPane) loader.load();
+            
+            rootLayout.setCenter(ventas);
+          
+            
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    
+}
